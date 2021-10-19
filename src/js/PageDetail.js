ba80310cac334f6e0348035cbbc64840ade2584c
jonathan justman <jojo.justman@gmail.com>
2021-10-19 18:04:21 +0200

    add autoprefixer

--- src/js/PageDetail.js
+++ src/js/PageDetail.js
@@ -16,13 +16,15 @@ function PageDetail(argument) {
       fetch(`${finalURL}`)
         .then((response) => response.json())
         .then((response) => {
-          let { name, released, description } = response;
+          let { name, released, description, background_image } = response;
+          console.log(response)
 
           let articleDOM = document.querySelector(".page-detail .article");
-
+          articleDOM.querySelector(".img-game img").src = background_image
           articleDOM.querySelector("h1.title").innerHTML = name;
-          articleDOM.querySelector("p.release-date span").innerHTML = released;
+          articleDOM.querySelector("p.release-date").innerHTML = released;
           articleDOM.querySelector("p.description").innerHTML = description;
+          articleDOM.querySelector("p.gameplay").innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aliquam quam veritatis cum molestias hic nesciunt autem quo repudiandae ipsum iste similique, numquam repellendus nobis ipsam, quos reprehenderit dolores accusamus"
         });
     };
 
@@ -33,9 +35,36 @@ function PageDetail(argument) {
     pageContent.innerHTML = `
       <section class="page-detail">
         <div class="article">
+          <div class="img-game">
+            <img>
+          </div>
           <h1 class="title"></h1>
-          <p class="release-date">Release date : <span></span></p>
-          <p class="description"></p>
+          <div>
+            <h3>Plot</h3>
+            <p class="description"></p>
+          </div>
+          <div>
+            <h3>Gameplay</h3>
+            <p class="gameplay"></p>
+          </div>
+          <div class="released-developer-platform-publisher">
+            <div>
+              <span>Released date:</span>
+              <p class="release-date"></p>
+            </div>
+            <div>
+              <span>Developer:</span>
+              <p class="developer"></p>
+            </div>
+            <div>
+              <span>Platform:</span>
+              <p class="platform"></p>
+            </div>
+            <div>
+              <span>Publisher:</span>
+              <p class="publisher"></p>
+            </div>
+          </div>
         </div>
       </section>
     `;
